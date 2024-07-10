f1f35481caf44d2a5ee8013a03326b0cff32e71f
Unnati Patel <unnati314@revature.net>
2024-07-10 17:07:31 -0400

    My First commit

---
+++ NewClass.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace MyNewApp{
+
+    class NewClass{
+        public static void Goodbye(){
+            Console.WriteLine("Thanks fro Using the app!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using MyNewApp;
+
+namespace MYApp{
+
+    class Program
+    {
+        static void Main(String[]args) {
+
+            Console.WriteLine("Enetr Name");
+            var name = Console.ReadLine();
+            Console.WriteLine("HEllo, " + name + "!");
+            Console.Write("How old are you= ");
+            var age= Console.ReadLine();
+            NewClass.Goodbye();
+        }
+    }
+}
